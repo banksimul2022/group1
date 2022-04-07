8653b24bd2fecbf95137e4af5d1d78c34274cf9d
Eemil <eemil.kauniskangas@gmail.com>
2022-04-07 17:47:46 +0300

    Add PIN and cardnubmer verification

--- API/models/kortti_model.js
+++ API/models/kortti_model.js
@@ -1,21 +1,26 @@
 const db = require('../database');
 const bcrypt = require('bcryptjs');
 
-const saltRounds=10;
-const Kortti={
-  get: function(callback) {
-    return db.query('select * from Kortti', callback); //haetaan paskaa
+const saltRounds = 10;
+const Kortti = {
+
+  CheckifCardExists: function (Korttinumero, callback) {
+    return db.query('select * from Kortti where Korttinumero=?', [Korttinumero], callback);
   },
-  getByCardNumber: function(Korttinumero, callback) { //haetaan kortin pin
+
+  get: function (callback) {
+    //do nothing
+  },
+  getPIN: function (Korttinumero, callback) { 
     return db.query('select PIN from Kortti where Korttinumero=?', [Korttinumero], callback);
   },
-  add: function(Kortti, callback) { //lisätään Kortti tietokantaan
-    bcrypt.hash(Kortti.PIN, saltRounds, function(err, hash) {
+  addCard: function (Kortti, callback) { //add card
+    bcrypt.hash(Kortti.PIN, saltRounds, function (err, hash) {
       return db.query('insert into Kortti (Korttinumero, PIN, Asiakas_IDAsiakas, Tili_idTili) values(?,?,?,?)',
-      [Kortti.Korttinumero, hash, Kortti.Asiakas_IDAsiakas, Kortti.Tili_idTili], callback);
+        [Kortti.Korttinumero, hash, Kortti.Asiakas_IDAsiakas, Kortti.Tili_idTili], callback);
     })
   }
 
 }
-          
+
 module.exports = Kortti;
